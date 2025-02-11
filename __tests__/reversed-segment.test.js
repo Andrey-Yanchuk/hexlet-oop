45e497da1d3518e3834181f8b2fbd7b404fdb04e
Yanchuk Andrey <orpiy@mail.ru>
2025-02-11 23:00:10 +0700

    test: add tests for toString() in Point and Segment

--- __tests__/reversed-segment.test.js
+++ __tests__/reversed-segment.test.js
@@ -20,25 +20,41 @@ describe("Testing the constructor-function Point", () => {
   test("with zero values", () => {
     expect(new Point(0, 0)).toEqual({ x: 0, y: 0 });
   });
+  describe("Testing Point.prototype.toString", () => {
+    test("with correctly values", () => {
+      expect(new Point(1, 10).toString()).toBe("(1, 10)");
+    });
+    test("with negative values", () => {
+      expect(new Point(11, -3).toString()).toBe("(11, -3)");
+    });
+    test("with empty values", () => {
+      expect(() => new Point().toString()).toThrow("x must be a finite number!");
+      expect(() => new Point(1).toString()).toThrow("y must be a finite number!");
+    });
+    test("with invalid values", () => {
+      expect(() => new Point("123", null).toString()).toThrow("x must be a finite number!");
+      expect(() => new Point(123, null).toString()).toThrow("y must be a finite number!");
+      expect(() => new Point(NaN, null).toString()).toThrow("x must be a finite number!");
+    });
+    test("with zero values", () => {
+      expect(new Point(0, 0).toString()).toBe("(0, 0)");
+    });
+  });
 });
 describe("Testing the constructor-function Segment", () => {
   test("with correctly values", () => {
     expect(new Segment(new Point(1, 10), new Point(11, 3))).toEqual({
       beginPoint: { x: 1, y: 10 },
       endPoint: { x: 11, y: 3 },
-      getBeginPoint: expect.any(Function),
-      getEndPoint: expect.any(Function),
     });
   });
   test("with negative values", () => {
     expect(new Segment(new Point(-1, -10), new Point(-11, -3))).toEqual({
       beginPoint: { x: -1, y: -10 },
       endPoint: { x: -11, y: -3 },
-      getBeginPoint: expect.any(Function),
-      getEndPoint: expect.any(Function),
     });
   });
-  test("with empty values", () => {
+  test("with empty arguments", () => {
     expect(() => new Segment()).toThrow(
       "beginPoint must be an instance of Point!",
     );
@@ -54,14 +70,36 @@ describe("Testing the constructor-function Segment", () => {
       "endPoint must be an instance of Point!",
     );
   });
+  describe("Testing Segment.prototype.toString", () => {
+    test("with correctly values", () => {
+      expect(new Segment(new Point(1, 10), new Point(11, 3)).toString()).toBe("[(1, 10), (11, 3)]");
+    });
+    test("with negative values", () => {
+      expect(new Segment(new Point(-1, -10), new Point(-11, -3)).toString()).toBe("[(-1, -10), (-11, -3)]");
+    });
+    test("with empty arguments", () => {
+      expect(() => new Segment().toString()).toThrow(
+        "beginPoint must be an instance of Point!",
+      );
+      expect(() => new Segment(new Point(-1, -10)).toString()).toThrow(
+        "endPoint must be an instance of Point!",
+      );
+    });
+    test("with invalid values", () => {
+      expect(() => new Segment("", {}).toString()).toThrow(
+        "beginPoint must be an instance of Point!",
+      );
+      expect(() => new Segment(new Point(-1, -10), null).toString()).toThrow(
+        "endPoint must be an instance of Point!",
+      );
+    });
+  });
 });
 describe("Testing the function reverse", () => {
   test("with correctly values", () => {
     expect(reverse(new Segment(new Point(1, 10), new Point(11, -3)))).toEqual({
       beginPoint: { x: 11, y: -3 },
       endPoint: { x: 1, y: 10 },
-      getBeginPoint: expect.any(Function),
-      getEndPoint: expect.any(Function),
     });
   });
   test("with negative values", () => {
@@ -70,8 +108,6 @@ describe("Testing the function reverse", () => {
     ).toEqual({
       beginPoint: { x: -11, y: -3 },
       endPoint: { x: -1, y: -10 },
-      getBeginPoint: expect.any(Function),
-      getEndPoint: expect.any(Function),
     });
   });
   test("with empty values", () => {
