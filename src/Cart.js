01410f4ac892ac35079f9a48e82d86180b3474ef
Yanchuk Andrey <orpiy@mail.ru>
2025-02-13 19:16:31 +0700

    feat: implement 'Cart' class

---
+++ src/Cart.js
@@ -0,0 +1,34 @@
+// src/Cart.js
+export class Cart {
+    constructor() {
+        this.items = [];
+    }
+    addItem(item, count = 1) {
+        if (count <= 0) throw new Error("Please enter the correct quantity of goods!");
+        if (!item.name || typeof item.name !== 'string') throw new Error("Please enter a valid name!");
+        if (typeof item.price !== 'number' || item.price < 0) throw new Error("A product cannot have a negative price!");
+        const existingEl = this.items.find((el) => el.item.name === item.name);
+        if (!existingEl) {
+            this.items.push({ item, count });
+        } else {
+            existingEl.count += count;
+        }
+    }
+    getItems() {
+        // Делаем для расширяемости кода, и чтобы нельзя было явно изменить код при его возврате
+        return this.items.map((el) => ({ ...el, item: { ...el.item } }));
+    }
+    getCost() {
+        return this.items.reduce((acc, el) => acc + el.item.price * el.count, 0);
+    }
+    getCount() {
+        return this.items.reduce((acc, el) => acc + el.count, 0);
+    }
+};
+const cart = new Cart();
+cart.addItem({ name: 'car', price: 3 }, 5);
+cart.addItem({ name: 'house', price: 10 }, 2);
+// console.log(cart.getItems().length); // 2
+// console.log(cart.getItems());
+// console.log(cart.getCost()); // 35
+// console.log(cart.getCount()); // 7
